443c022270142a3a41edd2b707ad5fad776579a5
Atanda1 <atandadave@gmail.com>
2023-10-27 20:06:59 +0100

    feat: nuxt icon demo

---
+++ app.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { users } from "/data/items-list.js";
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="wrapper__inner">
+      <Icon
+        class="wrapper__inner__icon"
+        name="logos:medium"
+        width="7em"
+        height="2.5em"
+        color="black"
+      />
+
+      <h3 class="wrapper__inner__title">What are you interested in?</h3>
+
+      <p class="wrapper__inner__select">Choose three or more.</p>
+    </div>
+    <div class="wrapper__options">
+      <div :key="user.id" v-for="user in users">
+        
+          <OptionButton :user="user" />
+        
+      </div>
+    </div>
+    <div class="wrapper__footer">
+      <button class="wrapper__footer__button">Continue</button>
+    </div>
+  </div>
+</template>
+
+
---
+++ components/OptionButton.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+  user: {
+    type: Array,
+    required: true,
+  },
+});
+const { user } = props;
+const toggleUser = ref(false);
+</script>
+<template>
+  <button
+    @click="toggleUser = !toggleUser"
+    :class="[
+      toggleUser && 'wrapper__options__button__active',
+      'wrapper__options__button',
+    ]"
+  >
+    <Icon
+      size="1.5em"
+      class="wrapper__options__button__avi"
+      :name="user.icon"
+    />
+    {{ user.username }}
+    <Icon
+      v-if="!toggleUser"
+      size="1.5em"
+      class="wrapper__options__button__plus"
+      name="clarity:plus-line"
+    />
+
+    <Icon
+      v-else
+      size="1.5em"
+      class="wrapper__options__button__plus"
+      name="iconamoon:check-light"
+      color="green"
+    />
+  </button>
+</template>
